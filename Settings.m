203b00a9b04aaa1803aefffdd019115268c80f39
Andreas Johansson <and.johansson93@gmail.com>
2015-11-23 19:11:29 +0100

    Added Settings file with different parameter categories.

---
+++ Settings.m
@@ -0,0 +1,17 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                       Settings
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Simulation parameters
+nIndividuals = 10;
+nTimeSteps = 10;
+
+% Room properties
+roomSize = [10,10];
+
+% Agent properties
+
+% Physics settings
+maxVelocity = 1;
+
+% Graphics settings
