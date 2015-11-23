2c8c76e8dd2aad1383d7231e9a25437dab04fa1d
Andreas Johansson <and.johansson93@gmail.com>
2015-11-23 19:12:40 +0100

    Expanded the basic structure of the program. Added TODO list.

--- Main.m
+++ Main.m
@@ -1,22 +1,47 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
- %Panic Project Main.m  
- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%  Panic Project Main.m  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
  
+%%%%%%%%%%%%%%%%%%%%% TODO List %%%%%%%%%%%%%%%%%%%%%%%%%%
+%TODO: IntializeAgents()
+%TODO: IntializeWalls()
+%TODO: Main loop structure
+%TODO: UpdateAcceleration()
+  % - Subfunctions for all forces
+%TODO: Calculate variable time step
+%TODO: UpdateVelocity()
+%TODO: UpdatePosition()
+%TODO: Graphics
+  % - InitializeGraphics()
+  % - UpdateGraphics()
+%TODO: 
 
- %%%%%%%%%%%%%%%%%%%%%%%Initilize%%%%%%%%%%%%%%%%%%%%%%%%%% 
- %CONSTANTS 
- nIndividuals = 10;
- maxVelocity = 1;%Remove??
- nTimeSteps = 10;
- roomSize = [10,10];
- %VECTORS 
- targetPositionVector = [1.5*roomSize(1),0.5*roomSize(2)];
- velocityVector = maxVelocity*rand(1,nIndividuals)
- %MATRICES 
-  structor  = struct('target Position Vector',targetPositionVector,...
+%%%%%%%%%%%%%%%%%%%%%% Initilize %%%%%%%%%%%%%%%%%%%%%%%%% 
+%  Read settings
+run('Settings.m');
+
+% Translation structor
+% IDEA: We could just use constants? 'POSITION = [1,2]' etc. Faster?
+translate = struct('Position',[1,2],...
+                   'Velocity',[3,4],...
+                   'Mass',5,...
+                   'Radius',6); % etc.
+
+%  Initialization
+% - Room (Walls)
+% - Desired position
+targetPosition = [1.5*roomSize(1),0.5*roomSize(2)];
+% - Agents
+agents = InitializeAgents(); 
+
+% velocityVector = maxVelocity*rand(1,nIndividuals) % In --> InitializeAgents()
+
+% MATRICES - Remove?
+structor  = struct('target Position Vector',targetPositionVector,...
      'Velocity vector',velocityVector);
- %%%%%%%%%%%%%%%%%%%%%%%TimeLoop%%%%%%%%%%%%%%%%%%%%%%%%%%% 
- for allTime = 1:nTimesteps
+   
+%%%%%%%%%%%%%%%%%%%%%% Main Loop %%%%%%%%%%%%%%%%%%%%%%%%% 
+for iTime = 1:nTimeSteps
  
- end 
- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+end 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
