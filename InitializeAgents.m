ea48de9e47274804fcaa1818442b98c7bdc63f6d
Andreas Johansson <and.johansson93@gmail.com>
2015-12-01 09:18:57 +0100

    Created file and added list of agent properties

---
+++ InitializeAgents.m
@@ -0,0 +1,16 @@
+function agents = InitializeAgents(nAgents,PROPERTIES,roomSize)
+
+  agents = zeros(nAgents,nProperties);
+  % Properties:
+  % - Position [1,2]
+  % - Velocity [3,4]
+  % - Mass [5]
+  % - Radius [6]
+  % - Desired speed [7]
+  % - Desired direction [8,9]
+  % - Desired time resolution [10]
+  % - Repulsion coefficient [11]
+  % - Repulsion exponent [12]
+  % - Impatience [13]
+
+end
