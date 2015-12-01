ab5e952ac689f031d2561f37ca23d6087bff53e0
Andreas Johansson <and.johansson93@gmail.com>
2015-12-01 11:08:50 +0100

    Removed unnecessary properties

--- UpdateAcceleration.m
+++ UpdateAcceleration.m
@@ -2,23 +2,11 @@ function acceleration = UpdateAcceleration(agents,walls,PROPERTIES,...
     bodyForceCoeff,frictionForceCoeff)
 
   % Read properties
-  position = agents(:,PROPERTIES.Position);
   velocity = agents(:,PROPERTIES.Velocity);
   mass = agents(:,PROPERTIES.Mass);
-  radius = agents(:,PROPERTIES.Radius);
   desiredSpeed = agents(:,PROPERTIES.DesiredSpeed);
   desiredDirection = agents(:,PROPERTIES.DesiredDirection);
   desiredTimeResolution = agents(:,PROPERTIES.DesiredTimeResolution);
-  repulsionCoeff = agents(:,PROPERTIES.RepulsionCoeff);
-  repulsionExp = agents(:,PROPERTIES.RepulsionExp);
-  impatience = agents(:,PROPERTIES.Impatience);  
-  
-  % Initialization
-  nAgents = size(agents,1);
-%   acceleration = zeros(nAgents,2); % Not needed if no loop over agents
-  desiredVelocityCorrection= zeros(nAgents,2);
-  agentForces = zeros(nAgents,2);
-  wallForces = zeros(nAgents,2);
   
   % Calculate acceleration components
   desiredVelocityCorrection = (repmat(desiredSpeed,1,2).*desiredDirection - ...
