ec74cb6c67f17648f53186d05ac301304922fabe
jakobnolkrantz <jakobn@student.chalmers.se>
2015-12-01 11:35:01 +0100

    Update wallGeneration.m

--- wallGeneration.m
+++ wallGeneration.m
@@ -1,8 +1,8 @@
-function walls=WallGeneration(roomSize,doorWidth,openingLength)
-openingLower=(roomSize(2)-doorWidth)/2;
-openingUpper=roomSize(2)-openingLower;
-walls=[roomSize(1)+openingLength 0;roomSize(1)+openingLength openingLower;roomSize(1) openingLower;roomSize(1) 0;0 0; 0 roomSize(2);...
+function walls = WallGeneration(roomSize,doorWidth,openingLength)
+  openingLower = (roomSize(2)-doorWidth)/2;
+  openingUpper = roomSize(2)-openingLower;
+  walls = [roomSize(1)+openingLength 0;roomSize(1)+openingLength openingLower;roomSize(1) openingLower;roomSize(1) 0;0 0; 0 roomSize(2);...
   roomSize(1) roomSize(2);roomSize(1) openingUpper;roomSize(1)+openingLength openingUpper;roomSize(1)+openingLength 1];
-importantPoints=[0 1 1 1 1 1 1 1 1 0]';
-walls=[walls(:,1) walls(:,2) importantPoints]
+  importantPoints = [0 1 1 1 1 1 1 1 1 0]';
+  walls = [walls(:,1) walls(:,2) importantPoints]
 end
