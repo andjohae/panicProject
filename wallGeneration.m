1d5d0b59fcb67815a73cb19615ca5d4f920bfb0b
jakobnolkrantz <jakobn@student.chalmers.se>
2015-12-01 11:32:03 +0100

    Update wallGeneration.m

--- wallGeneration.m
+++ wallGeneration.m
@@ -5,6 +5,4 @@ walls=[roomSize(1)+openingLength 0;roomSize(1)+openingLength openingLower;roomSi
   roomSize(1) roomSize(2);roomSize(1) openingUpper;roomSize(1)+openingLength openingUpper;roomSize(1)+openingLength 1];
 importantPoints=[0 1 1 1 1 1 1 1 1 0]';
 walls=[walls(:,1) walls(:,2) importantPoints]
-plot(walls(1:2,1),walls(1:2,2),'LineWidth',2);
-axis([-roomSize(1)*0.25 roomSize(1)*1.25 -roomSize(2)*0.25 roomSize(2)*1.25])
 end
