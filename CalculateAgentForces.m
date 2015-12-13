1d6f1bd068e459eadb31ea67d614e18aa749c730
Andreas Johansson <and.johansson93@gmail.com>
2015-12-13 16:22:30 +0100

    Actually calculates the sum of forces

--- CalculateAgentForces.m
+++ CalculateAgentForces.m
@@ -13,38 +13,42 @@ function agentForces = CalculateAgentForces(agents, PROPERTIES, bodyForceCoeff,.
   % Initialization
   nAgents = size(agents,1);
   agentForces = zeros(nAgents,2);
-  repulsionForce = zeros(nAgents,2);
-  bodyForce = zeros(nAgents,2);
-  frictionForce = zeros(nAgents,2);
   
   % Loop over all agents
   for i = 1:nAgents
+    repulsionForce = zeros(nAgents,2);
+    bodyForce = zeros(nAgents,2);
+    frictionForce = zeros(nAgents,2);
+    
     for j = 1:nAgents
-      
       if (i ~= j) % Can't interact with oneself  
         direction = position(i,:)-position(j,:);
         distance = norm(direction);
         unitDirection = direction./distance;
         radiiSum = radius(i)+radius(j);
         
-        repulsionForce(i,:) = repulsionCoeff(i) * exp((radiiSum-distance)/...
+        % Calculate repulsion force
+        repulsionForce(j,:) = repulsionCoeff(i) * exp((radiiSum-distance)/...
             repulsionExp(i)) .* unitDirection;
         
         if (distance <= radiiSum) % Check for collision
           % Calculate body force
-          bodyForce(i,:) = bodyForceCoeff * (radiiSum - distance) .* unitDirection;
+          bodyForce(j,:) = bodyForceCoeff * (radiiSum - distance) .* unitDirection;
           
           % Calculate friction force
           tangent = [-unitDirection(2), unitDirection(1)];
           relativeTangentialVelocity = (velocity(j,:)-velocity(i,:)) * tangent'; % Equivalent to scalar product?
-          frictionForce(i,:) = frictionForceCoeff * (radiiSum - distance) * ...
+          frictionForce(j,:) = frictionForceCoeff * (radiiSum - distance) * ...
               relativeTangentialVelocity * tangent;
         end
-      else
-        agentForces(i,:) = [0,0];
       end
-      
     end
+    
+    % Expansion: check if body forces exceed injury tolerence
+    
+    % Calculate sum of interaction forces
+    agentForces(i,:) = sum(repulsionForce,1) + sum(bodyForce,1) + sum(frictionForce,1);
+    
   end
   
 end
