0fc50cc1e7508f3467eb1b1ecf0a1284d7516a6f
nc1224 <nc1244@gmail.com>
2015-12-13 18:00:56 +0100

    Update Main.m

--- Main.m
+++ Main.m
@@ -30,34 +30,42 @@ figure(1)
 clf
 hold on
 plot(walls(:,1),walls(:,2),'LineWidth',2);
-plot(targetPosition(1),targetPosition(2),'x')
-axis([-roomSize(1)*0.25 roomSize(1)*1.7 -roomSize(2)*0.25 roomSize(2)*1.25])
+plot(targetPosition(1),targetPosition(2),'x','MarkerSize',150)
+
+  axis([-roomSize(1)*0.25 roomSize(1)*1.7 -roomSize(2)*0.25 roomSize(2)*1.25])
+
 % - Desired position
 % - Agents
 agents = InitializeAgents(nAgents,PROPERTIES,meanMass,meanRadius,...
-  targetPosition,roomSize,initialVelocity);
+  targetPosition,roomSize);
 
 % velocityVector = maxVelocity*rand(1,nIndividuals) % In --> InitializeAgents()
 
 % MATRICES - Remove?
 
 
-%%%%%%%%%%%%%%%%%%%%%% Main Loop %%%%%%%%%%%%%%%%%%%%%%%%%
-for iTime = 1:nTimeSteps
-    currentAcceleration=UpdateAcceleration(agents,walls,PROPERTIES,...
-  bodyForceCoeff,frictionForceCoeff);
+%%%%%%%%%%%%%%%%%%%%%% MainLoop %%%%%%%%%%%%%%%%%%%%%%%%%
+for iTime = 1:nTimeSteps*100
+  currentAcceleration=UpdateAcceleration(agents,walls,PROPERTIES,...
+    bodyForceCoeff,frictionForceCoeff);
   
-  desiredDirection=bsxfun(@minus,agents(:,PROPERTIES.Position),targetPosition);
-  desiredDirection=desiredDirection/norm(desiredDirection)
-  speedInDesiredDirection=speedInDesiredDirection+sum(agents(:,PROPERTIES.Velocity).*(desiredDirection),2);
+  desiredDirection=bsxfun(@minus,targetPosition,agents(:,PROPERTIES.Position));
+  agents(:,PROPERTIES.DesiredDirection)=desiredDirection/norm(desiredDirection,2);
+  speedInDesiredDirection=speedInDesiredDirection+sum(agents(:,PROPERTIES.Velocity).*(agents(:,PROPERTIES.DesiredDirection)),2);
   
   agents(:,PROPERTIES.Velocity)=agents(:,PROPERTIES.Velocity)+currentAcceleration.*deltaTime;
   agents(:,PROPERTIES.Position)=agents(:,PROPERTIES.Position)+ agents(:,PROPERTIES.Velocity).*deltaTime;
-  %agents(:,PROPERTIES.DesiredSpeed)=ImpatienceUpdate(agents(:,PROPERTIES.Velocity),agents(:,PROPERTIES.DesiredSpeed),speedInDesiredDirection,iTime);
+  agents(:,PROPERTIES.DesiredSpeed)=ImpatienceUpdate(agents, PROPERTIES,speedInDesiredDirection,iTime,maxDesiredVelocity)
   clf
   hold on
+	
+      axis([-roomSize(1)*0.25 roomSize(1)*1.7 -roomSize(2)*0.25 roomSize(2)*1.25])
+
   plot(walls(:,1),walls(:,2),'LineWidth',2);
+  plot(targetPosition(1),targetPosition(2),'x','MarkerSize',20)
+
   plot(agents(:,PROPERTIES.Position(1)),agents(:,PROPERTIES.Position(2)),'.','MarkerSize',30);
+
   drawnow update;
   
 end
