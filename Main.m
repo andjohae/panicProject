a8fb8130e87283d320b216a4f3f88ce7d60ac43a
nc1224 <nc1244@gmail.com>
2015-12-13 15:56:03 +0100

    Update Main.m

--- Main.m
+++ Main.m
@@ -1,7 +1,7 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  Panic Project Main.m
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
- 
+
 %%%%%%%%%%%%%%%%%%%%% TODO List %%%%%%%%%%%%%%%%%%%%%%%%%%
 %TODO: IntializeAgents()
 %TODO: IntializeWalls()
@@ -12,8 +12,8 @@
 %TODO: UpdateVelocity()
 %TODO: UpdatePosition()
 %TODO: Graphics
-  % - InitializeGraphics()
-  % - UpdateGraphics()
+% - InitializeGraphics()
+% - UpdateGraphics()
 %TODO: Implement impatience. In UpdateAcceleration or elsewhere?
 
 %%%%%%%%%%%%%%%%%%%%%% Initilize %%%%%%%%%%%%%%%%%%%%%%%%%
@@ -22,7 +22,8 @@ run('Parameters.m');
 
 %  Initialization
 targetPosition = [1.5*roomSize(1),0.5*roomSize(2)];
-speedInDesiredDirection=zeros(1,nAgents);
+speedInDesiopeningLower = (roomSize(2)-doorWidth)/2;
+redDirection=zeros(1,nAgents);
 
 % - Room (Walls)
 walls = WallGeneration(roomSize,doorWidth,openingLength);
@@ -35,26 +36,26 @@ axis([-roomSize(1)*0.25 roomSize(1)*1.7 -roomSize(2)*0.25 roomSize(2)*1.25])
 % - Desired position
 % - Agents
 agents = InitializeAgents(nAgents,PROPERTIES,meanMass,meanRadius,...
-  targetPosition,roomSize); 
+  targetPosition,roomSize);
 
 % velocityVector = maxVelocity*rand(1,nIndividuals) % In --> InitializeAgents()
 
 % MATRICES - Remove?
 structor  = struct('target Position Vector',targetPositionVector,...
-     'Velocity vector',velocityVector);
-   
-%%%%%%%%%%%%%%%%%%%%%% Main Loop %%%%%%%%%%%%%%%%%%%%%%%%% 
+  'Velocity vector',velocityVector);
+
+%%%%%%%%%%%%%%%%%%%%%% Main Loop %%%%%%%%%%%%%%%%%%%%%%%%%
 for iTime = 1:nTimeSteps
-   currentAcceleration=UpdateAcceleration(agents,walls,PROPERTIES,...
+  currentAcceleration=UpdateAcceleration(agents,walls,PROPERTIES,...
     bodyForceCoeff,frictionForceCoeff);
-    desiredDirection=bsxfun(@minus,agents(:,Properties.Position),targetPosition);
-    desiredDirection=desiredDirection/norm(desiredDirection);
-    speedInDesiredDirection=speedInDesiredDirection+agents(:,Properties.Velocity)*(desiredDirection);
+  desiredDirection=bsxfun(@minus,agents(:,Properties.Position),targetPosition);
+  desiredDirection=desiredDirection/norm(desiredDirection);
+  speedInDesiredDirection=speedInDesiredDirection+agents(:,Properties.Velocity)*(desiredDirection);
   agents(:,PROPERTIES.Velocity)=agents(:,PROPERTIES.Velocity)+currentAcceleration.*deltaTime;
   agents(:,PROPERTIES.Position)=agents(:,PROPERTIES.Position) +currentVelocity.*deltaTime;
   agents(:,PROPERTIES.DesiredSpeed)=ImpatienceUpdate(agents(:,PROPERTIES.Velocity),agents(:,PROPERTIES.DesiredSpeed),speedInDesiredDirection,iTime);
-end 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
 clf
 run('Parameters.m');
