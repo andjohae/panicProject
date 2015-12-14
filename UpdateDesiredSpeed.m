f26dd177d0910d98a79119c4231cc9d66d09bb6e
Andreas Johansson <and.johansson93@gmail.com>
2015-12-14 12:35:33 +0100

    Changed some matlab cmds that didn't work

--- UpdateDesiredSpeed.m
+++ UpdateDesiredSpeed.m
@@ -1,13 +1,18 @@
 function newDesiredSpeed = UpdateDesiredSpeed(agent, PROPERTIES, ...
-    speedInDesiredDirection,iTime)
+    speedInDesiredDirection,iTime, maxDesiredSpeed)
  
   % Read properties
   velocity = agents(:,PROPERTIES.Velocity);
   desiredSpeed = agents(:,PROPERTIES.DesiredSpeed);
+  impatience = agents(:,PROPERTIES.Impatience);
   
-  % Calculate new desired speed
-  meanVelocity=speedInDesiredDirection/iTime;
+%   Calculate new desired speed
+  meanVelocity = speedInDesiredDirection/iTime;
   impatience = 1 -meanVelocity./agentVelocity;
-  newDesiredSpeed = (1-impatience)*initialVelocity +impatience*aesiredSpeed;
+  
+  agents(:,PROPERTIES.Impatience) = 1 - (speedInDesiredDirection./iTime) ./ ...
+      agents(:,PROPERTIES.Velocity);
+  
+  newDesiredSpeed = (1-impatience)*initialVelocity +impatience*desiredSpeed;
 
 end
