e6c198d8a2934092ffbfe3ce2ab0cfbc71ee3325
Andreas Johansson <and.johansson93@gmail.com>
2015-12-14 14:20:45 +0100

    Removed old plot functions

--- code/Main.m
+++ code/Main.m
@@ -27,12 +27,6 @@ speedInDesiredDirection=zeros(nAgents,1);
 
 % - Room (Walls)
 walls = WallGeneration(roomSize,doorWidth,openingLength);
-% figure(1)
-% clf
-% hold on
-% plot(walls(:,1),walls(:,2),'LineWidth',2);
-% plot(targetPosition(1),targetPosition(2),'x','MarkerSize',150)
-% axis([-roomSize(1)*0.25 roomSize(1)*1.7 -roomSize(2)*0.25 roomSize(2)*1.25])
 
 % - Agents
 agents = InitializeAgents(nAgents,PROPERTIES,meanMass,meanRadius,...
