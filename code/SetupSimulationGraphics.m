9160789e3b252b318060b4db66b241d01e366c61
nc1224 <nc1244@gmail.com>
2015-12-16 21:37:33 +0100

    Update SetupSimulationGraphics.m

--- code/SetupSimulationGraphics.m
+++ code/SetupSimulationGraphics.m
@@ -4,14 +4,24 @@ hFig1 = figure(1);
 clf(hFig1);
 
 % agentArea = pi .* agents(:,PROPERTIES.Radius).^2;
-agentArea = (2 .* agents(:,PROPERTIES.Radius)).^2;
+%agentArea = (2 .* agents(:,PROPERTIES.Radius)).^2;
+
+sizeAdjustment=agents(:,PROPERTIES.Radius).*1.28;
+
 
 hold on;
 hWallPlot = plot(walls(:,1),walls(:,2),'LineWidth',2,'Color','k');
 hTargetPlot = plot(targetPosition(1),targetPosition(2),'rx','MarkerSize',10);
 hSocialPlot = gplot(socialCorrelations ,agents(:,PROPERTIES.Position),'k-');
-hAgentPlot = scatter(agents(:,PROPERTIES.Position(1)),agents(:,PROPERTIES.Position(2)),...
-    agentArea);
+hAgentPlot = scatter(agents(:,PROPERTIES.Position(1)),agents(:,PROPERTIES.Position(2)),'filled');
+%Oklar kod nedanför
+currentunits = get(gca,'Units');
+set(gca, 'Units', 'Points');
+axpos = get(gca,'Position');
+set(gca, 'Units', currentunits);
+markerWidth = sizeAdjustment/diff(xlim)*axpos(3);
+set(hAgentPlot, 'SizeData', markerWidth.^2)
+
 hold off;
 
 box on;
