3203edc5d60e08e51c0f9dcc21a40512355a1716
nc1224 <nc1244@gmail.com>
2015-12-17 15:14:17 +0100

    Update SetupSimulationGraphics.m

--- code/SetupSimulationGraphics.m
+++ code/SetupSimulationGraphics.m
@@ -25,4 +25,4 @@ markerWidthY = sizeAdjustment/diff(ylim)*axpos(4);
 set(hAgentPlot, 'SizeData', markerWidthX.*markerWidthX)
 hTimeStamp = text(-5, -7, sprintf('Time: %d',0));
 set(hTimeStamp, 'Interpreter','Latex','FontSize',14);
-drawnow;
+drawnow; /n
