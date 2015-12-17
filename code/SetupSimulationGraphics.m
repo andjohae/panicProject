b3292932a47be75ff8625997148c0bcea08015f3
nc1224 <nc1244@gmail.com>
2015-12-17 15:19:47 +0100

    Update SetupSimulationGraphics.m

--- code/SetupSimulationGraphics.m
+++ code/SetupSimulationGraphics.m
@@ -3,7 +3,7 @@
 hFig1 = figure(1);
 clf(hFig1);
 
-sizeAdjustment=0.3*1.6;
+sizeAdjustment=1.43*1.6;
 hold on;
 hWallPlot = plot(walls(:,1),walls(:,2),'LineWidth',2,'Color','k');
 hTargetPlot = plot(targetPosition(1),targetPosition(2),'rx','MarkerSize',10);
@@ -18,11 +18,11 @@ set(gca,'XTick',[]);
 set(gca,'YTick',[]);
 currentunits = get(gca,'Units');
 set(gca, 'Units', 'Points');
-axpos = get(gca,'Position')
+axpos = get(gca,'Position');
 set(gca, 'Units', currentunits);
 markerWidthX = sizeAdjustment/diff(xlim)*axpos(3);
 markerWidthY = sizeAdjustment/diff(ylim)*axpos(4);
-set(hAgentPlot, 'SizeData', markerWidthX.*markerWidthX)
+set(hAgentPlot, 'SizeData', markerWidthX.*markerWidthY)
 hTimeStamp = text(-5, -7, sprintf('Time: %d',0));
 set(hTimeStamp, 'Interpreter','Latex','FontSize',14);
 drawnow;
