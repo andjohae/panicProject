4e2ff234ca61073b077c8fd33e4aad5c13af5b65
Helst Vadsom <forsberg.timmy@live.com>
2016-01-06 14:22:19 +0100

    Create GetGraphsFromData.m

---
+++ GetGraphsFromData.m
@@ -0,0 +1,49 @@
+function [] =  GetGraphsFromData(times,survivors)
+%Assumes input in MATLAB format.
+%load/save is good for that?
+
+[nVel,leT] = size(times);
+
+%Fix Data
+for j = 1:nVel
+  desiredVelocity = times(1,j);
+  time = times(j,:);
+  for i = 1:leT
+    if time(i) == 0
+      time(i) = [];
+    end
+  end
+  
+  %Total time plot
+  figure(4)
+  hold on
+  if survivors(j) ==length(time)
+    plot(desiredVelocity,time(end),'g*')
+  else
+    plot(desiredVelocity,time(end),'r*')
+  end
+  text(desiredVelocity,time(end),num2str(survivors(j)))
+  
+  %Mean time plot
+  figure(5)
+  hold on
+  if survivors(j) ==length(time)
+    plot(desiredVelocity,sum(time)/length(time),'g*')
+  else
+    plot(desiredVelocity,sum(time)/length(time),'r*')
+  end
+  text(desiredVelocity,sum(time)/length(time),num2str(survivors(j)))
+  
+  %Label
+  figure(4)
+  xlabel('Desiered Velocity v_0 [m/s]');
+  ylabel('Total Escape Time [t]');
+  label('Number of Survivors');
+  grid on;
+  figure(5)
+  xlabel('Desiered Velocity v_0 [m/s]');
+  ylabel('Mean Escape Time [t]');
+  label('Number of Survivors');
+  grid on;
+  hold off
+end
